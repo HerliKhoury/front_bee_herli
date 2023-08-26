329a27316a2c03fda20008ac19faa32f46b8efdb
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-08-26 18:49:44 -0300

    Feature: Project Structure & Schemas

---
+++ src/Schemas/property.schemas.ts
@@ -0,0 +1,20 @@
+import * as yup from "yup";
+
+export const propertySchema = yup.object().shape({
+    name: yup.string().required("Nome obrigatório!"),
+    total_area: yup.string().required("Área total obrigatória!"),
+    built_area: yup.string().required("Área construida obrigatória!"),
+    address: yup.string().required("Endereço obrigatório!"),
+    zip_code: yup.string().required("CEP obrigatório!")
+    .matches(/^\d{5}-\d{2}$/, "O CEP deve estar no formato 00000-00"),
+    price: yup.string().required("Preço obrigatório!")
+});
+
+export const propertySchemaOptional = yup.object().shape({
+    name: yup.string(),
+    total_area: yup.string(),
+    built_area: yup.string(),
+    address: yup.string(),
+    zip_code: yup.string().matches(/^\d{5}-\d{2}$/, "O CEP deve estar no formato 00000-00"),
+    price: yup.string()
+});
