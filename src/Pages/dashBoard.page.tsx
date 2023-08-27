eb29602687526c8813cae80c419221d950d775ec
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-08-27 18:39:16 -0300

    Feature: Dash board page

---
+++ src/Pages/dashBoard.page.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../Components/Header/Header.component";
+
+export const DashBoard = () => {
+    return(
+        <div>
+            <Header userName="Herli" userEmail="meu@mail.com"/>
+            {/* lógica dos cards ou vazio */}
+        </div>
+    )
+};
