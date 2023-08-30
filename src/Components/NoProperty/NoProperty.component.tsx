44adaa1c503e7a6b03640b4599f4e2bafe13ba3b
Herli Khoury <92793769+HerliKhoury@users.noreply.github.com>
2023-08-30 09:02:26 -0300

    Feature: No property style

--- src/Components/NoProperty/NoProperty.component.tsx
+++ src/Components/NoProperty/NoProperty.component.tsx
@@ -1,10 +1,11 @@
 import noPropertiesImg from "../../Assets/house_closed.png";
+import "./noProperty.style.css";
 
 export const NoProperty = () => {
     return(
-        <div>
-            <img src={noPropertiesImg} alt="Casa Fechada"/>
-            <h1>Você não possui imóveis cadastrados</h1>
+        <div className="no-property-div">
+            <img className="no-property-img" src={noPropertiesImg} alt="Casa Fechada"/>
+            <h1 className="no-property-info">Você não possui imóveis cadastrados</h1>
         </div>
     );
 };
